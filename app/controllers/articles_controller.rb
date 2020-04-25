daac4cd32985e027d67184619033061f25d8fa00
yash-dani <daniyash19@gmail.com>
2020-04-24 20:58:19 -0400

    added delete function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,4 +15,8 @@ class ArticlesController < ApplicationController
         @article.save
         redirect_to article_path(@article)
     end
+    def destroy
+        @article = Article.destroy(params[:id])
+        redirect_to action: "index"
+    end
 end
