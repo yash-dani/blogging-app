28721d4e2e2399f3c0ac8633f6107c4befdf046a
yash-dani <daniyash19@gmail.com>
2020-04-24 20:25:26 -0400

    finished new blog creation feature

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,18 @@
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+    def show
+        @article = Article.find(params[:id])
+    end
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.new(
+            title: params[:article][:title],
+            body: params[:article][:body])
+        @article.save
+        redirect_to article_path(@article)
+    end
+end
