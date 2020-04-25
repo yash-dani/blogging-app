96e2f5856e55b22b3e7f4b52f14e2d5c73692a00
yash-dani <daniyash19@gmail.com>
2020-04-25 19:24:46 -0400

    added editing blogs feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,4 +19,17 @@ class ArticlesController < ApplicationController
         @article = Article.destroy(params[:id])
         redirect_to action: "index"
     end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        redirect_to article_path(@article)
+    end
+    private
+    def article_params
+        params.require(:article).permit(:title, :body) 
+    end
+
 end
